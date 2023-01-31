fa89ef390cb7aefc0092230906c7fb2e8966e15c
Erika Camacho <luciac1103@gmail.com>
2023-01-31 18:05:36 -0500

    Creación de los paquetes y las clases básicas en el proyecto

---
+++ src/main/java/co/edu/unicauca/productos/controllers/ProductoRestController.java
@@ -0,0 +1,59 @@
+package co.edu.unicauca.productos.controllers;
+
+import co.edu.unicauca.productos.models.ProductoEntity;
+import co.edu.unicauca.productos.services.DTO.ProductoDTO;
+import co.edu.unicauca.productos.services.IProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api")
+public class ProductoRestController {
+    @Autowired
+    private IProductoService productoService;
+
+    @GetMapping("/productos")
+    public List<ProductoDTO> index() {
+        return productoService.findAll();
+    }
+
+    @GetMapping("/clientes/{id}")
+    public ProductoDTO show(@PathVariable Integer codigo) {
+        ProductoDTO objProducto = null;
+        objProducto = productoService.findById(codigo);
+        return objProducto;
+    }
+    @PostMapping("/clientes")
+    public ProductoDTO create(@RequestBody ProductoDTO producto) {
+        ProductoDTO objProducto = null;
+        objProducto =  productoService.save(producto);
+        return objProducto;
+    }
+    @PutMapping("/productos/{codigo}")
+    public ProductoDTO update(@RequestBody ProductoDTO producto, @PathVariable Integer codigo) {
+        ProductoDTO objProducto = null;
+        ProductoDTO productoActual = productoService.findById(codigo);
+        if(productoActual!=null)
+        {
+            objProducto = productoService.update(codigo,producto);
+        }
+        return objProducto;
+    }
+    @DeleteMapping("/productos/{codigo}")
+    public Boolean delete(@PathVariable Integer codigo) {
+        Boolean bandera=false;
+        ProductoDTO productoActual = productoService.findById(codigo);
+        if(productoActual!=null)
+        {
+            bandera = productoService.delete(codigo);
+        }
+        return bandera;
+
+    }
+}
