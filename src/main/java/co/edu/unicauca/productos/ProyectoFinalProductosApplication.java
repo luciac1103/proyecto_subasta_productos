69d63f9815dfa2a374d3bee43b4d8bf0016c7e32
Erika Camacho <luciac1103@gmail.com>
2023-01-31 16:08:18 -0500

    Primer commit

---
+++ src/main/java/co/edu/unicauca/productos/ProyectoFinalProductosApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalProductosApplication.class, args);
+	}
+
+}
